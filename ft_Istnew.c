5bb4fec55b3adcedff531e7c38def0eff4a8bd77
Cynthia Luu <cyluu@e1z3r4p4.42.us.org>
2019-08-20 18:13:13 -0700

    commit

--- ft_Istnew.c
+++ ft_Istnew.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/20 17:09:52 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/20 17:35:25 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/20 18:12:54 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 t_list  *   ft_lstnew(void const *content, size_t content_size)
 {
-    t_list *new;
+    t_list *newlk;
 
-    t_list = (* new)malloc(size of content && content_size);
-    if t_list (*new) = NULL
-    free(*new);  
-    if content = "\0"
-        Return (content(*new))
-    if !content_size = 0
-        Return (content_size(*new))
+    newlk = (t_list *)malloc(content_size);
+
+    if (t_list (*newlk) == NULL)
+        free(*newlk);  
+    if (!content || !content_size)
+        return (NULL);
+    if (content == '\0')
+        return (*newlk);
+    if (content_size == 0)
+        return (*newlk);
     else
     {
-        return(*new)
+        return(newlk);
     }
-    
-}
-
 }
