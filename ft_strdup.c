240c22a68631bc1e515ce348cfa07e993392799f
Cynthia Luu <cyluu@e1z3r6p25.42.us.org>
2019-08-20 23:49:54 -0700

    add

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_Istnew.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/20 17:09:52 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/20 19:06:16 by cyluu            ###   ########.fr       */
+/*   Created: 2019/08/20 22:38:28 by cyluu             #+#    #+#             */
+/*   Updated: 2019/08/20 23:32:41 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *   ft_lstnew(void const *content, size_t content_size)
+#include <string.h> 
+
+char  *   ft_strdup(const char *s1, size_t n)
 {
-    t_list *newlk;
-
-    *newlk = (t_list *)malloc(content_size(*newlk));
-
-    if (t_list (*newlk) == NULL)
-        free(*newlk);  
-    if (!content || !content_size)
-        return (NULL);
-    if (content == '\0')
-        return (*newlk);
-    if (content_size == 0)
-        return (*newlk);
-    else
-    {
-        return(*newlk);
-    }
+    
 }
