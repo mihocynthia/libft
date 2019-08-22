ada2532c5cca0f97576d22fe6998b6f05a4f8a0c
Cynthia Luu <cyluu@e1z3r4p4.42.us.org>
2019-08-22 16:23:53 -0700

    commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 11:45:42 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/22 15:47:21 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/22 16:13:07 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 #include <string.h>
 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void *ft_memcpy(unsigned char *dst, unsigned char *src, size_t n)
 {
     size_t i;
     i = 0;
-    
+
     if (n = 0 || src[i] = dst[i])
-        return (dst[i])
+        return (dst[i]);
     
     while (n > i)  
     {
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,17 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 14:25:49 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/22 15:18:26 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/22 16:13:04 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void *ft_memmove(unsigned char *dst, unsigned char *src, size_t len)
 {
+int i;
+i = 0;
 
+while (len != 0)
+ src[i](len) = dst[i](len);
+    i++;
+return (dst)
 }
