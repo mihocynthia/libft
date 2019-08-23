1e0836b7c4ac3853bd568bdbc01ec2676fd39db6
Cynthia Luu <cyluu@e1z3r4p4.42.us.org>
2019-08-22 17:29:48 -0700

    commit

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 17:01:03 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/22 17:14:54 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/22 17:29:30 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 
     i = 0;
     buff = malloc(sizeof(*char) * n)
+    src[i++](buff) = dst[i++](buff)
+    free(dst);
     
 }   
