9e24e07ed75a10b7122b2d717fc5d47b128ece66
Cynthia Luu <cyluu@e1z3r5p24.42.us.org>
2019-08-27 11:15:46 -0700

    commit

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,39 +6,39 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 17:01:03 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/26 17:11:52 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/27 11:13:30 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* The memccpy() function copies bytes from memory s2 into s1, stopping after
+** the first occurence of c has been copied, or after n bytes have been copied, 
+** which ever comes first. 
+*/
 
 #include "libft.h"
 
 #include <string.h>
 
-void *ft_memccpy(void *dst, const void *src, int c, size_t n)
+void    *ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
+    unsigned char *src_ret;
+    unsigned char *dst_ret;
+    unsigned char c_ret;
     int i;
-    const char *src_c;
-    char *dst_c;
-    
-    i = 0;
-    src_c = src;
-    dst_c = dst;
-    while (n-- && src_c[i])
-    {
-        dst_c[i] = src_c[i];
-        if (src_c[i] == c)
-            return(dst + (i + 1));
-        i++;
-    }
-    return (NULL);
-}
 
-// int main()
-// {
-//     char src[] = "a";
-//     char dst[50] = "zxcvbfghjk";
-//     // char *tst = ft_memccpy(dst, src, 'b', 7);
-//     char *ans = memccpy(dst, src, 'b', 7);
-//     // printf("tst|%s|\n", tst);
-//     printf("ans|%s|\n", ans);
-// }
+    i = -1;
+    src_ret = (unsigned char *)src;
+    dst_ret = (unsigned char *)dst;
+    c_ret = (unsigned char)c;
+
+    while (n--)
+    {
+        i++;
+        dst_ret[i] = c_ret;
+        if (src_ret[i] == c_ret)
+        {
+            return((void *)dst_ret);
+        }
+    return ((void *)dst_ret);
+    }
+}
