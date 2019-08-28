eae356fec224bea888aa7fc72e9c99ce42ac374d
Cynthia Luu <cyluu@e1z1r6p4.42.us.org>
2019-08-27 21:53:06 -0700

    commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 11:45:42 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/24 15:11:37 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/27 21:52:41 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
     const char *src_c;
 
     i = 0;
-    dst_c = dst;
-    src_c = src;
+    
+    dst_c = (char *)dst;
+    src_c = (const char *)src;
+    
     if (n == 0)
         return (dst_c);
     
