415939e8b898a751fe7c9b83ff0cddeebd64cb74
Cynthia Luu <cyluu@e1z1r6p1.42.us.org>
2019-08-29 02:04:07 -0700

    commit

--- ft_striter.c
+++ ft_striter.c
@@ -6,26 +6,23 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 21:19:59 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/28 21:43:31 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/29 02:03:01 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* Applies the function f to each index of the array 
-*pointed to s.
+*pointed to s. string to pointer
 */
 
 void ft_striter(char *s, void (*f)(char *))
 {
     int i;
 
-    while (f != '\0' || s != '\0' )
-    return;
-    if 
-
-    (char *)
-    {
-
-    }
+    if (s && f != NULL)
+    return ;
+    while (s[i] != NULL)
+       f(s[i]);
+       i++;
 }   
