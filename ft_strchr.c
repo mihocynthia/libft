6c26c0b93483a504c885482485f758488a15aae1
Cynthia Luu <cyluu@e1z3r6p25.42.us.org>
2019-08-29 15:07:50 -0700

    commit

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,27 +6,29 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 17:27:28 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/27 11:51:38 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/29 15:07:13 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//7 failed 3 okays 
-char *ft_strchr(const char *s, int c)
+/*
+** 7 failed 3 okays
+*/
+char	*ft_strchr(const char *s, int c)
 {
-    int i;
+	int		i;
+	char	*s_ret;
+	
+	i = 0;
+	s_ret = (char*)s;
 
-    i = 0;
-    char *s_ret;
-    s_ret = (char*)s;
-
-    while (i++ && s_ret[i])
-    {
-        if(s_ret[i] == c)
-            return(&s_ret[i]);
-    }
-    if(s_ret[i] != '\0')
-        return (&s_ret[i]);
-    return (NULL);
+	while (i++ && s_ret[i])
+	{
+		if (s_ret[i] == c)
+			return (&s_ret[i]);
+	}
+	if (s_ret[i] != '\0')
+		return (&s_ret[i]);
+	return (NULL);
 }
