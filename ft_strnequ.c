f2584def61fe09ad6467f921433237ecb41f7e2b
Cynthia Luu <cyluu@e1z1r6p4.42.us.org>
2019-08-28 17:50:04 -0700

    commit

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/28 17:02:32 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/28 17:20:22 by cyluu            ###   ########.fr       */
+/*   Created: 2019/08/28 17:40:53 by cyluu             #+#    #+#             */
+/*   Updated: 2019/08/28 17:49:50 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Lexicographical comparison between s1 and s2. If the 2 strings are 
-* identical the function returns 1, or 0 otherwise.
+/* Lexicographical comparison between s1 and s2 up to n char- acters or 
+* until a ’\0’ is reached. If the 2 strings are identical, the function 
+* returns 1, or 0 otherwise.
  */
 
-int     ft_strequ(char const *s1, char const *s2)
+#include "libft.h"
+
+int     ft_strnequ(char const *s1, char const *s2, size_t n)
 {
     int i;
     i = 0;
+    char *len;
+    len = (char)n;
     ft_strcmp(s1, s2);
-    while (s1[i])
+    while (s1[i] && s2[i] > len < s2[i] && s1[i])
         if (s1[i] == s2[i])
         {
             return (1);
