b05886a50e0f6906b00cefc16e20884ddab410b9
Cynthia Luu <cyluu@e1z3r1p12.42.us.org>
2019-08-30 03:11:30 -0700

    commit

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,28 +1,26 @@
 
-
 #include "libft.h"
 
 char *ft_strncat(char *s1, const char *s2, size_t n)
 {
-    size_t i;
-    size_t j;
-    unsigned char *s1_return;
+	size_t i;
+	size_t j;
+	unsigned char *s1_return;
 	unsigned char *s2_return;
-    
-    i = 0;
-    j = 0;
+
+	i = 0;
+	j = 0;
 	s1_return = (unsigned char *)s1;
 	s2_return = (unsigned char *)s2;
-    while (s1_return[i] != '\0')
-    {
-            i++;
-    } 
-    while (s2_return[j] != '\0' && j < (n - 1))
-    {
-        s1_return[i++] = s2_return[j++];
-    }
-    s1_return[i] = s2_return[j];
-    
-    return ((char *)s1_return);
-} 
+	while (s1_return[i] != '\0')
+	{
+		i++;
+	} 
+	while (s2_return[j] != '\0' && j < (n - 1))
+	{
+		s1_return[i++] = s2_return[j++];
+	}
+	s1_return[i] = s2_return[j];
 
+	return ((char *)s1_return);
+} 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 14:31:36 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/29 20:40:31 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/30 02:59:37 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ char    *ft_strcat(char *s1, const char *s2);
 t_list *ft_lstnew(void const *content, size_t content_size);
 void *ft_memmove(void *dst, const void *src, size_t len);
 void    ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char    *ft_strjoin(char const *s1, char const *s2);
 
 
