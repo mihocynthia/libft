75ce8c642717be34101e189a50159c103d0388f8
Cynthia Luu <cyluu@e1z3r6p25.42.us.org>
2019-08-29 20:12:29 -0700

    swap files deleted

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,15 +6,15 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 03:05:12 by cyluu             #+#    #+#             */
-/*   Updated: 2019/08/29 03:13:18 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/08/29 20:05:48 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Allocates (with malloc(3)) and returns an array of “fresh” strings (all 
-* ending with ’\0’, including the array itself) ob- tained by spliting s using 
-* the character c as a delimiter. If the allocation fails the function returns 
-* NULL. Example : ft_strsplit("*hello*fellow***students*", ’*’) re- turns the 
-* array ["hello", "fellow", "students"].
+/* Allocates (with malloc(3)) and returns an array of “fresh” strings (all
+**ending with ’\0’, including the array itself) ob- tained by spliting s using
+**the character c as a delimiter. If the allocation fails the function returns
+**NULL. Example : ft_strsplit("*hello*fellow***students*", ’*’) re- turns the
+**array ["hello", "fellow", "students"].:wq
  */
 char ** ft_strsplit(char const *s, char c);
 
