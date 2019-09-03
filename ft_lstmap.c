f6784482d6796f1c676c76e02d88faaed7a25532
Cynthia Luu <cyluu@e1z2r5p47.42.us.org>
2019-09-02 19:44:11 -0700

    commit

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,15 +6,16 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 16:37:15 by cyluu             #+#    #+#             */
-/*   Updated: 2019/09/02 08:59:12 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/09/02 19:43:51 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*fresh;
+	t_list		*fresh;
+
 	fresh = (t_list*)malloc(sizeof(t_list));
 	if (lst)
 	{
