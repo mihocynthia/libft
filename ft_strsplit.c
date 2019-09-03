bc15a638c6e270e2ff617edf1ba4837d719b9af5
Cynthia Luu <cyluu@e1z2r5p47.42.us.org>
2019-09-02 23:53:25 -0700

    commit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 03:05:12 by cyluu             #+#    #+#             */
-/*   Updated: 2019/09/02 23:40:45 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/09/02 23:53:16 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	**ft_strsplit(char const *s, char c)
 	int		r1;
 
 	r1 = 0;
-	if (!s && !t)
+	if (!s)
 		return (NULL);
 	t = (char **)ft_memalloc(sizeof (char *) * (ft_countword(s, c) + 1));
-	if (t == NULL)
+	if (!t)
 		return (NULL);
 	while (*s != '\0')
 	{
