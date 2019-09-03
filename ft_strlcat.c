fda93c6b4479521a4d9a3df141d9846620bd8934
Cynthia Luu <cyluu@e1z3r8p8.42.us.org>
2019-09-03 15:02:43 -0700

    commit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cyluu <cyluu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 23:12:40 by cyluu             #+#    #+#             */
-/*   Updated: 2019/09/03 00:10:44 by cyluu            ###   ########.fr       */
+/*   Updated: 2019/09/03 15:02:12 by cyluu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 **appends string src to the end of dst.  It will append at most
 **dstsize - strlen(dst) - 1 characters.  It will then NUL-terminate, unless
 **dstsize is 0 or the original dst string was longer than dstsize (in practice
-**this should not happen as it means that either dstsize is incorrect or that
-**dst is not a proper string).
+**this is not a proper string).
 */
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dsize)
